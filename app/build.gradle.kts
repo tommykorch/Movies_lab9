d04646b3033a58894375797ad7d8c6c44e2decda
tommykorch <tkorchmit@bk.ru>
2024-12-28 10:30:07 +0700

    Ошибка в дублировании библиотек. Решение: annotationProcessor ("androidx.room:room-compiler:2.6.0")

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("androidx.room:room-compiler:2.6.0")
+    annotationProcessor ("androidx.room:room-compiler:2.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
